79d7fb70ef803e294a4d652ef455875cff2c9494
bora <nhatbaoxxd@gmail.com>
2024-03-28 16:27:49 +0700

    Folder division and naming

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
 import express from 'express'
-import userRouter from '@/user.routes'
+import usersRouter from '@/routes/users.routes'
 
 const app = express()
-
 const port = 3000
 
-// middleware that is specific to userRouter
-userRouter.use(
-  (req, res, next) => {
-    console.log('Time: ', Date.now())
-    next()
-  },
-  (req, res, next) => {
-    console.log('Time: ', Date.now())
-    next()
-  }
-)
-
-app.use('/api', userRouter)
+app.use(express.json())
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+
+import { loginValidator } from '@/middlewares/users.middlewares'
+import { loginController } from '@/controllers/users.controllers'
+
+const usersRouter = Router()
+
+usersRouter.post('/login', loginValidator, loginController)
+
+export default usersRouter
