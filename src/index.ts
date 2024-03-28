ab989f6b04e6fe6c2515c6072a069fdae4056a70
bora <nhatbaoxxd@gmail.com>
2024-03-28 15:23:50 +0700

    Routing in Express.js

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-const name = 'Dư Thanh Được'
-console.log(name)
+import express from 'express'
+import userRouter from '@/user.routes'
+
+const app = express()
+
+const port = 3000
+
+// middleware that is specific to userRouter
+userRouter.use(
+  (req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+  },
+  (req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+  }
+)
+
+app.use('/api', userRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
