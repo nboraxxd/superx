b02e8f401d60e940df0642248533329423955fa4
bora <nhatbaoxxd@gmail.com>
2024-03-31 01:41:37 +0700

    Refactor user registration in users.controllers.ts

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express'
 
-import User from '@/models/schemas/User.schema'
-import databaseService from '@/services/database.services'
+import usersService from '@/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
   res.json({ message: 'User logged in' })
@@ -11,11 +10,10 @@ export const registerController = async (req: Request, res: Response) => {
   const { email, password } = req.body
 
   try {
-    const result = await databaseService.users.insertOne(new User({ email, password }))
+    const result = await usersService.register({ email, password })
 
-    return res.status(201).json({ message: 'User registered', data: result })
+    return res.status(201).json({ message: 'User registered', result })
   } catch (error) {
-    console.log('🔥 ~ registerController ~ error:', error)
     return res.status(400).json({ message: 'User registration failed', error })
   }
 }
